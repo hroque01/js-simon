c1400c2651a267e46180b9b992cb27307973faca
hroque01 <hadson.roque01@gmail.com>
2022-11-20 12:18:33 +0100

    Consegna esercizio

--- main.js
+++ main.js
@@ -28,3 +28,40 @@ console.log(numArray)
     //ora prendo il contenitore e ci stampo i numeri
     numArray.forEach( element => cont.innerHTML += `<span class="box">${element}</span>`)
 
+    //dichiaro il contenitore dei risultati
+    const numRes = document.querySelector('.results');
+
+function simonSays () {
+    // array dell'utente
+    let arrayUser = [];
+    
+    //nascondo i numeri dell'utente con una classe displaynone
+    cont.classList.add("hidden");
+    numRes.classList.remove('hidden');
+
+    //faccio inserire all'utente 5 numeri con 1 prompt, questo lo faccio tranmite un ciclo
+
+    setTimeout(() => {
+        for ( let i = 0; i < 5; i++ ) {
+        
+            let userNum = parseInt(prompt("Inserisci il numero"));
+    
+            // se il numero e' uguale ai numeri generati lo salvo
+            if (numArray.includes(userNum)) {
+                arrayUser.push(userNum);
+            } else {
+                
+            }
+        }
+    
+        // stampo i numeri ricordati dall'utente
+        if (arrayUser == '') {
+            numRes.innerHTML = `<h3>Non hai indovinato nessun numero!</h3>`;
+        } else {
+            arrayUser.forEach(element => numRes.innerHTML += `<span class="box">${element}</span>`);
+        }
+    }, 0);
+
+}
+
+setTimeout(simonSays, 30000);
